a984a37ba98f2c22bb6ebfebb2ec8918a63678d7
Beth Sebian <esebian@gmail.com>
2016-01-11 20:31:25 -0700

    Add visitor cart
    Visit can now add items to cart, with flash notice that item has been added
    and updated count of items in their cart.

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,11 @@
+class CartItemsController < ApplicationController
+	include ActionView::Helpers::TextHelper
+
+	def create
+		item = Item.find(params[:item_id])
+		@cart.add_item(item.id)
+		session[:cart] = @cart.contents
+		flash[:notice] = "You have #{pluralize(@cart.count_of, "item")} in your cart."
+		redirect_to '/items'
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :items, only: [:index]
   end
 
+  resources :cart_items, only: [:create]
 end
