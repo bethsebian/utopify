915d0ed5bc9a3fe6b823e979d1fd9ed734f1dbf2
Beth Sebian <esebian@gmail.com>
2016-01-14 21:43:55 -0700

    Remove unused methods in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,3 @@
 class Item < ActiveRecord::Base
   belongs_to :travesty
-
-  def create
-    Item.create(params[:item_params])
-  end
-
-private
-
-  def item_params
-    params.require(:item).permit(:title, :description, :price, :image_url, :travesty_id)
-  end
 end
