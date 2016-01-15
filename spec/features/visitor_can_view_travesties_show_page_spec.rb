459e8341bd622c5fd57b19b6c5b41852703c0d64
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-15 11:05:37 -0700

    Merge Beth's refactor_thurs branch, fixed conflicts

--- spec/features/visitor_can_view_travesties_show_page_spec.rb
+++ spec/features/visitor_can_view_travesties_show_page_spec.rb
@@ -30,7 +30,6 @@ RSpec.feature "Visitor navigates to travesty page" do
       expect(page).to have_content("$4,523.00")
       expect(page).to have_content(item_4.title)
       expect(page).to have_content(item_4.description)
-      expect(page).to have_content(item_4.price)
       expect(page).to have_content("$8,765.00")
     end
 
