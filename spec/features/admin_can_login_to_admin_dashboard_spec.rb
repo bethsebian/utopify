fdb78a9b69f059f6b6899dbfd958cb36a7b1377f
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-16 13:23:48 -0700

    Add test to ensure default user cannot login to admin_dashboard
    Added test to ensure that the default users are not redirected
    to the admin_dashboard_path upon logging in.

--- spec/features/admin_can_login_to_admin_dashboard_spec.rb
+++ spec/features/admin_can_login_to_admin_dashboard_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Admin can login" do
   scenario "Admin user is redirected to admin dashboard when logging in" do
-    admin = User.create(first_name: "mister", last_name: "admin", username: "admin", password: "admin", role: 1)
+    admin = User.create(first_name: "mister",
+                        last_name: "admin",
+                        username: "admin",
+                        password: "admin",
+                        role: 1)
 
     visit login_path
 
@@ -15,4 +19,22 @@ RSpec.feature "Admin can login" do
     expect(page).to have_content "Admin Dashboard"
   end
 
+  scenario "default user does not see admin dashboard when logging in" do
+    user = User.create(first_name: "default",
+                       last_name: "user",
+                       username: "default_user",
+                       password: "default",
+                       role: 0 )
+
+    visit login_path
+
+    fill_in "Username", with: "default_user"
+    fill_in "Password", with: "default"
+
+    click_button "Submit"
+
+    expect(current_path).to_not eq admin_dashboard_path
+    expect(page).to_not have_content "Admin Dashboard"
+    expect(current_path).to eq dashboard_path
+  end
 end
