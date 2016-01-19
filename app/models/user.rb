2e2efe404c449a09d431af2187c832df128646c1
Beth Sebian <esebian@gmail.com>
2016-01-19 13:16:14 -0700

    Add error message when creating an account with claimed username
    Add sad test, update validation message in user model, and tweaked flash setup in
    users_controller.
    
    User controller set flash.now[:error], changed to flash[:error]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { message: "%{value} is already taken. Please choose a different username"}
   enum role: %w(default admin)
 end
--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -27,4 +27,28 @@ RSpec.feature "user can create account from home" do
     expect(page).to_not have_content "Login"
     expect(page).to have_content "Logout"
   end
+
+  scenario "is redirected to account page if username already exists" do
+    user = User.create(
+      first_name: "Jordan",
+      last_name: "Lawler",
+      username: "jlawlz",
+      password: "password"
+    )
+
+    visit new_user_path
+
+    fill_in "First Name:", with: "Justin"
+    fill_in "Last Name:", with: "Lawlerz"
+    fill_in "Username:", with: "jlawlz"
+    fill_in "Password:", with: "password1"
+    fill_in "Password Confirmation", with: "password1"
+
+    within "form#new_user" do
+      click_button "Create"
+    end
+
+    expect(current_path).to eq new_user_path
+    expect(page).to have_content "Username #{user.username} is already taken. Please choose a different username"
+  end
 end
