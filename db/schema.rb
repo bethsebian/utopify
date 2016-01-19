f356c785bc4a912eb4788d76d4cc1851f8167e29
Jordan Lawler <jordan.lawler@colorado.edu>
2016-01-19 08:56:50 -0700

    Added Sponsers to Travesties
    Sponsers now have a sponser_image, and sponser_title column in the database. sent through to carousel view for display.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118163846) do
+ActiveRecord::Schema.define(version: 20160119154027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,11 @@ ActiveRecord::Schema.define(version: 20160118163846) do
 
   create_table "travesties", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "slug"
+    t.string   "sponsor_image"
+    t.string   "sponsor_title"
   end
 
   create_table "users", force: :cascade do |t|
