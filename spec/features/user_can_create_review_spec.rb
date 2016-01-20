41078c9109a7f69ece1a11a2bdccdc4fb1c1c13d
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-19 17:42:41 -0700

    Add reviews to user dashboard
    Added all a user's reviews to that user's dashboard page

--- spec/features/user_can_create_review_spec.rb
+++ spec/features/user_can_create_review_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.feature "User can create a review for an item" do
   scenario "User is taken back to item after creating a review for it" do
     travesty = create(:travesty)
-    item = travesty.items.create(title: "Black Rhino", description: "kill em all", price: 10)
-    user = User.create(first_name: "Greg", last_name: "Armstrong", password: "password", username: "greg", role: 0)
+    item = travesty.items.create(title: "Black Rhino",
+                                description: "kill em all",
+                                price: 10)
+    user = User.create(first_name: "Greg",
+                       last_name: "Armstrong",
+                       password: "password",
+                       username: "greg",
+                       role: 0)
     text = "Test Review"
 
     ApplicationController.any_instance.stub(:current_user).and_return(user)
@@ -17,9 +23,33 @@ RSpec.feature "User can create a review for an item" do
     fill_in "Text", with: text
     click_on "Create Review"
 
-    save_and_open_page
-
     expect(page).to have_content(user.username)
     expect(page).to have_content(text)
+  end
+
+  scenario "user can see all their reviews on their dashboard" do
+    travesty = create(:travesty)
+    item = travesty.items.create(title: "Black Rhino",
+                                description: "kill em all",
+                                price: 10)
+    user = User.create(first_name: "Greg",
+                       last_name: "Armstrong",
+                       password: "password",
+                       username: "greg",
+                       role: 0)
+    review = Review.create(text: "Test Review")
+    item.reviews << review
+    user.reviews << review
+
+    visit login_path
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_on "Sign In"
+
+    save_and_open_page
+
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_content(review.text)
   end
 end
