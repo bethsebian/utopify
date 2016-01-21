8c9878639383821413ddebf1a6ae686ee67df02c
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-20 22:19:15 -0700

    Add default image to item creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120162311) do
+ActiveRecord::Schema.define(version: 20160121051327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20160120162311) do
     t.string   "title"
     t.string   "description"
     t.integer  "price"
-    t.string   "image_url"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "image_url",   default: "bill_cosby.gif"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.integer  "travesty_id"
     t.boolean  "active",      default: true
   end
