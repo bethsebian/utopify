30e2b75046f7c6c93d00f3d9b66597744890596e
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-20 17:52:51 -0700

    second round of heroku fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'facoty_girl_rails'
+require 'factory_girl_rails'
 
 Shoulda::Matchers.configure do |config|
  config.integrate do |with|
