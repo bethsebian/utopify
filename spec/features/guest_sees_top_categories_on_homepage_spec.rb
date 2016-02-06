c811562bacf85e6fa4ac248d57163451d38cf9c0
Beth Sebian <esebian@gmail.com>
2016-02-06 16:33:10 -0700

    Add top categories to homepage

--- spec/features/guest_sees_top_categories_on_homepage_spec.rb
+++ spec/features/guest_sees_top_categories_on_homepage_spec.rb
@@ -2,42 +2,37 @@ require 'rails_helper'
 
 RSpec.feature "guest visits item page" do
 	scenario "and sees item details" do
-		pending
 		category_1 = create(:category_with_items, items_count: 1)
 		category_2 = create(:category_with_items, items_count: 1)
 		category_3 = create(:category_with_items, items_count: 1)
 		category_4 = create(:category_with_items, items_count: 1)
 
-		item_1 = category_1.items
-		item_2 = category_2.items
-		item_3 = category_3.items
-		item_4 = category_4.items
+		item_1 = category_1.items.first
+		item_2 = category_2.items.first
+		item_3 = category_3.items.first
+		item_4 = category_4.items.first
 
 		order_1 = create(:order)
 		order_2 = create(:order)
 		order_3 = create(:order)
 		order_4 = create(:order)
 
-		order_1 << item_1 * 1
-		order_2 << item_2 * 2
-		order_3 << item_3 * 3
-		order_4 << item_4 * 4
+		order_1.items << [item_4]
+		order_2.items << [item_4, item_3]
+		order_3.items << [item_4, item_3, item_2]
+		order_4.items << [item_4, item_3, item_2, item_1]
 
 		visit root_path
 
 		within "#home-middle" do
-			expect(page).to have_content(category_2.title)
-			expect(page).to have_content(category_3.title)
-			expect(page).to have_content(category_4.title)
-			expect(page).to_not have_content(category_1.title)
+			expect(page).to have_link category_2.title,
+  			href: category_path(category_2)
+			expect(page).to have_link category_3.title,
+  			href: category_path(category_3)
+			expect(page).to have_link category_4.title,
+  			href: category_path(category_4)
+			expect(page).to_not have_link category_1.title,
+  			href: category_path(category_1)
 		end
 	end
 end
-
-
-# As a guest,
-# when I visit the homepage, and
-# there are more than 3 categories,
-# I see cards for the 3 most popular categories, and
-# when I click on a category,
-# I am directed to that category’s index page.
