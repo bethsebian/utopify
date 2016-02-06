286a67e60b496734ba17a6e4e38a9e1edb2eb50e
Justin Pease <justinpease2@gmail.com>
2016-02-06 15:13:10 -0700

    adds test for user visting an items store

--- spec/features/guest_shopping_navigation_spec.rb
+++ spec/features/guest_shopping_navigation_spec.rb
@@ -47,6 +47,28 @@ RSpec.feature "guest visits item page for other items in item show page category
 		expect(current_path).to eq(category_path(item_1.category.slug))
   end
 
+	scenario "guest can go back to the store for the item they are looking at" do
+		store_1, store_2 = create_list(:store, 2)
+		category_1 = create(:category_with_items, items_count: 5)
+		category_2 = create(:category_with_items, items_count: 1)
+		item_1, item_2, item_3, item_4, item_5 = category_1.items
+		item_6, item_7, item_8, item_9, item_10 = category_2.items
+		store_1.items << [item_1, item_2, item_3, item_4]
+		store_1.items << item_6
+		store_2.items << item_5
+
+		visit store_item_path(store_1, store_1.items[0])
+		expect(page).to have_content(item_1.title)
+		expect(current_path).to eq(store_item_path(store_1, store_1.items[0]))
+
+		within ('#feature-details-dynamic') do
+			click_on "#{item_1.store.title}"
+		end
+
+		expect(current_path).to eq(store_path(item_1.store))
+
+	end
+
 	scenario "guest can go back to the home page after browsing item pages" do
 		store_1, store_2 = create_list(:store, 2)
 		category_1 = create(:category_with_items, items_count: 5)
@@ -72,5 +94,5 @@ end
 # Card 3 - Guest can return to home page
 # As a guest/user,
 # when I visit item show page, and
-# when I click on Store Home Page,
-# then I am redirected to Store Home Page
+# when I click on Site Home Page,
+# then I am redirected to Site Home Page
