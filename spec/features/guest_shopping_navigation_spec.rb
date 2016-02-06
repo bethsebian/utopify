d08f555d89bf1632c01cafcd174f360b5c56df5b
Justin Pease <justinpease2@gmail.com>
2016-02-06 14:25:06 -0700

    adds test to navigate back to home page

--- spec/features/guest_shopping_navigation_spec.rb
+++ spec/features/guest_shopping_navigation_spec.rb
@@ -17,7 +17,6 @@ RSpec.feature "guest visits item page for other items in item show page category
 		expect(page).to have_content(item_1.category.title)
 		within "#home-middle" do
 			expect(page).to have_content(item_2.title)
-			save_and_open_page
       click_on("link-to-item-#{item_2.id}")
 		end
 
@@ -46,8 +45,26 @@ RSpec.feature "guest visits item page for other items in item show page category
     click_link "#{item_1.category.title}"
 
 		expect(current_path).to eq(category_path(item_1.category.slug))
-
   end
+
+	scenario "guest can go back to the home page after browsing item pages" do
+		store_1, store_2 = create_list(:store, 2)
+		category_1 = create(:category_with_items, items_count: 5)
+		category_2 = create(:category_with_items, items_count: 1)
+		item_1, item_2, item_3, item_4, item_5 = category_1.items
+		item_6, item_7, item_8, item_9, item_10 = category_2.items
+		store_1.items << [item_1, item_2, item_3, item_4]
+		store_1.items << item_6
+		store_2.items << item_5
+
+		visit store_item_path(store_1, store_1.items[0])
+		expect(page).to have_content(item_1.title)
+		expect(current_path).to eq(store_item_path(store_1, store_1.items[0]))
+
+		click_on 'Utopify'
+		expect(current_path).to eq(root_path)
+
+	end
 end
 
 
@@ -57,15 +74,3 @@ end
 # when I visit item show page, and
 # when I click on Store Home Page,
 # then I am redirected to Store Home Page
-#
-# Card 4 - Guest can move to category index page
-# As a guest/user,
-# when I visit item show page, and
-# when I click on Category name,
-# then I redirected to Category index page
-#
-# Card 5 - Guest can view other items in category
-# As a guest/user,
-# when I visit item show page, and
-# when I click on an item in “Other items in [Category]:
-# then I’m redirected to that item Show Page
