8e4991f66a770acc7d862a47ec6ee666a43ec97c
Justin Pease <justinpease2@gmail.com>
2016-02-08 22:26:43 -0700

    not sure what I'm adding

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,9 @@
 class Store < ActiveRecord::Base
   before_save :generate_slug
-  has_many :items
   has_one :user
+  has_many :items
+  has_many :order_items, through: :items
+  has_many :orders, through: :order_items
 
   def generate_slug
     self.slug = title.parameterize
