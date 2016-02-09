7f2c3e7c2f3e544a59702f74bd203a17c135c823
Beth Sebian <esebian@gmail.com>
2016-02-08 22:27:22 -0700

    Add test for functionality: user adds items to cart from store page

--- spec/features/user_interacts_with_cart_spec.rb
+++ spec/features/user_interacts_with_cart_spec.rb
@@ -25,4 +25,29 @@ RSpec.feature "user interacts with cart" do
 		end
 		expect(page).to have_content("My Cart: 3")
 	end
+
+	scenario "from store page" do
+		item_1, item_2 = create(:category_with_items, items_count: 2).items
+		store = create(:store)
+
+		item_1.store = store
+		item_1.save
+		item_2.store = store
+		item_2.save
+
+		visit store_path(store.slug)
+
+		expect(page).to have_content("My Cart: 0")
+
+		within "div.item_#{item_1.id}" do
+			click_button('Add To Cart')
+		end
+		expect(page).to have_content("My Cart: 1")
+
+		within ".item_#{item_2.id}" do
+			fill_in "cart_item[quantity]", with: "2"
+			click_on "Add To Cart"
+		end
+		expect(page).to have_content("My Cart: 3")
+	end
 end
