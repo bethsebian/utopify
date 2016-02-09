c3c78b265896142b7e185efc7d5fc2d6286914a8
Justin Pease <justinpease2@gmail.com>
2016-02-09 08:30:15 -0700

    removes byebug

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -1,6 +1,5 @@
 class Stores::ItemsController < ApplicationController
   def show
-    # binding.pry
     @item = Item.find_by(id: params[:id])
     @related_items = @item.category.items.where(store_id: @item.store_id).last(3)
   end
