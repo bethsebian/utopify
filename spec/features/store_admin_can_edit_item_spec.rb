bd7623a77f3feaad0bec002ea3f79be58d035f16
Justin Pease <justinpease2@gmail.com>
2016-02-10 10:44:48 -0700

    store admin can edit an item and is happy and sad path tested

--- spec/features/store_admin_can_edit_item_spec.rb
+++ spec/features/store_admin_can_edit_item_spec.rb
@@ -28,9 +28,9 @@ RSpec.feature "admin visits dashboard clicks link to edit item" do
 			expect(page).to have_content(store.items.first.description)
       click_on "Edit"
 		end
-    expect(current_path).to eq(edit_item_path)
+    expect(current_path).to eq(edit_item_path(item_1))
 
-    within("#new-item-form") do
+    within("#edit-item-form") do
     	fill_in "Title", with: "New Item"
     	fill_in "Description", with: "New description"
     	fill_in "Image Url", with: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi33_2fiezKAhVnuYMKHXjtANkQjRwIAw&url=http%3A%2F%2Fjosephcphillips.com%2F2015%2F07%2Fof-course-bill-cosby-is-guilty%2F&psig=AFQjCNEMITizMWYxf5CZgZDU6BWWlr6a6w&ust=1455155083470723	"
@@ -41,11 +41,13 @@ RSpec.feature "admin visits dashboard clicks link to edit item" do
 		expect(current_path).to eq(store_dashboard_index_path(store.slug))
 
 		within(".store-items-dashboard-table") do
-			expect(page).to have_content(store.items.second.description)
+			expect(page).to have_content("New description")
 		end
+		expect(page).to have_content("Your item was successfully edited")
+
   end
 
-	scenario "fills in the form wrong and is redirected to the new item page" do
+	scenario "fills in the form wrong and is redirected to the edit item page" do
 		category_1 = create(:category_with_items, items_count: 1)
 
 		item_1 = category_1.items.first
@@ -70,19 +72,10 @@ RSpec.feature "admin visits dashboard clicks link to edit item" do
 		within(".store-items-dashboard-table") do
 			expect(page).to have_content(store.items.first.title)
 			expect(page).to have_content(store.items.first.description)
-      click_on "Add an item"
+      click_on "Edit"
 		end
-    expect(current_path).to eq(new_item_path)
+    expect(current_path).to eq(edit_item_path(item_1))
+  end
 
-		within("#new-item-form") do
-			fill_in "Title", with: nil
-			fill_in "Description", with: nil
-			fill_in "Image Url", with: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi33_2fiezKAhVnuYMKHXjtANkQjRwIAw&url=http%3A%2F%2Fjosephcphillips.com%2F2015%2F07%2Fof-course-bill-cosby-is-guilty%2F&psig=AFQjCNEMITizMWYxf5CZgZDU6BWWlr6a6w&ust=1455155083470723	"
-			# select "value[2]", :from => "item[category]"
-			click_on "Submit"
-		end
-		expect(current_path).to eq(new_item_path)
-
-	end
 
 end
