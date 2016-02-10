04ed48ad54533b3adfc977ebbff74c2f69dabf62
Obtuse <theobtuseautodidact@gmail.com>
2016-02-09 17:37:48 -0700

    platform admin can view store admin dashboards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,11 @@ class DashboardController < ApplicationController
     if current_user.store == Store.find_by_slug(params["store_slug"])
       @store = current_user.store
       @orders = @store.orders
+    elsif current_user.platform_admin?
+      @store = Store.find_by_slug(params["store_slug"])
+      @orders = @store.orders
     else
       redirect_to 'public/404'
     end
-
   end
-
 end
--- spec/features/only_approp_store_admin_views_dashboard_spec.rb
+++ spec/features/only_approp_store_admin_views_dashboard_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature "store admin vists wrong store dash" do
-	scenario "and is redirected to the home page" do
+RSpec.feature "store and store's admin' exist" do
+  attr_reader :store_admin_1, :store_admin_2, :store_1, :store_2, :registered_user, :platform_admin
 
-    store_admin_1 = create(:user, role: 1)
-    store_1 = create(:store, user_id: store_admin_1.id)
+  def setup
+    @platform_admin = create(:user, role: 2)
 
-    store_admin_2 = create(:user, role: 1)
-    store_2 = create(:store, user_id: store_admin_2.id)
+    @store_admin_1 = create(:user, role: 1)
+    @store_1 = create(:store, user_id: store_admin_1.id)
+
+    @store_admin_2 = create(:user, role: 1)
+    @store_2 = create(:store, user_id: store_admin_2.id)
+
+    @registered_user = create(:user, role: 0)
+  end
+
+	scenario "store_admin cannot access other store's dash" do
+    setup
 
     ApplicationController.any_instance.stub(:current_user).and_return(store_admin_1)
 
@@ -16,9 +25,34 @@ RSpec.feature "store admin vists wrong store dash" do
     expect(page).to have_content(store_1.title)
 
     visit store_dashboard_index_path(store_2.slug)
-    
+
     expect(current_path).to eq('/404')
     expect(page).to have_content("doesn't exist")
-
   end
+
+  scenario "user cannot access store dash" do
+    setup
+
+    ApplicationController.any_instance.stub(:current_user).and_return(registered_user)
+
+    visit store_dashboard_index_path(store_1.slug)
+
+    expect(current_path).to eq('/404')
+    expect(page).to have_content("doesn't exist")
+  end
+
+  scenario "platform_admin can view any store's dash" do
+    setup
+
+    ApplicationController.any_instance.stub(:current_user).and_return(platform_admin)
+
+    visit store_dashboard_index_path(store_1.slug)
+
+    expect(page).to have_content(store_1.title)
+
+    visit store_dashboard_index_path(store_2.slug)
+
+    expect(page).to have_content(store_2.title)
+  end
+
 end
