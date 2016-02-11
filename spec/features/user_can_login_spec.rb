302a69857c89fe06aa4f393fd495827d4ea9d979
Justin Pease <justinpease2@gmail.com>
2016-02-10 18:42:01 -0700

    user logins in is tested and user entering the wrong password is tested

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,37 @@
+# As a user,
+# When I’m on the homepage,
+# and when I click on login,
+# I am on the login page,
+# and when I enter my username and password and click submit,
+# I am redirected to my dashboard.
+require 'rails_helper'
+
+RSpec.feature "guest visits home page and clicks on the login button" do
+	scenario "user fills in proper info and is routed to their dashboard" do
+    user = create(:user)
+    visit root_path
+    click_on "Login"
+
+    fill_in "Username", :with => user.username
+    fill_in "Password", :with => "password"
+    click_on "Sign In"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Logged in as #{user.first_name}")
+  end
+
+  scenario "user fills in improper info `and sees an error message" do
+    user = create(:user)
+    visit root_path
+    click_on "Login"
+
+    fill_in "Username", :with => user.username
+    fill_in "Password", :with => "this is a wrong password"
+    click_on "Sign In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Username or password incorrect.")
+
+  end
+
+end
