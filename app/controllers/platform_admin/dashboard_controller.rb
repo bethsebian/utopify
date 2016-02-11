ac2e0f49ac1e56093cc10f0a051f66ea02d54e7b
Justin Pease <justinpease2@gmail.com>
2016-02-10 17:56:52 -0700

    breaks out platform dashboard page into partials

--- app/controllers/platform_admin/dashboard_controller.rb
+++ app/controllers/platform_admin/dashboard_controller.rb
@@ -5,9 +5,6 @@ class PlatformAdmin::DashboardController < ApplicationController
   def index
     @categories = Category.all
     @stores = Store.all
-    # @declined_stores = Store .where(status: "declined")
-    # @pending_stores = Store.where(status: "pending")
-    # @active_stores = Store.where(status: "active")
   end
 
 end
