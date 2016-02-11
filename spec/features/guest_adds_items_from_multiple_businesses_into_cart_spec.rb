8807b42be36d1d1ae7d55ce5f4b1256cb4b9998e
Obtuse <theobtuseautodidact@gmail.com>
2016-02-10 21:42:16 -0700

    test guest ability to place items from multiple businesses into single cart, tests passing

--- spec/features/guest_adds_items_from_multiple_businesses_into_cart_spec.rb
+++ spec/features/guest_adds_items_from_multiple_businesses_into_cart_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "guest visits store show page" do
 	scenario "and sees top items and item details" do
 		store_1 = Store.create(title: "test", accreditations: ["Hurray"])
 		store_2 = Store.create(title: "bad store", accreditations: ["booooo"])
-    store_3 = Store.create(title: "third store", acrreditations: ["people like us"])
+    store_3 = Store.create(title: "third store", accreditations: ["people like us"])
 		category_1 = create(:category_with_items, items_count: 6)
 		item_1, item_2, item_3, item_4, item_5, item_6 = category_1.items
 		store_1.items << [item_1, item_2, item_3]
@@ -12,3 +12,27 @@ RSpec.feature "guest visits store show page" do
     store_3.items <<[item_3, item_6]
 
     visit root_path
+
+    expect(page).to have_content("My Cart: 0")
+
+    first(:link, store_1.title).click
+    first(:button, "Add To Cart").click
+    click_on("Utopify")
+    first(:link, store_2.title).click
+    first(:button, "Add To Cart").click
+    click_on("Utopify")
+    first(:link, store_3.title).click
+    first(:button, "Add To Cart").click
+
+    expect(page).to have_content("My Cart: 3")
+
+    click_on("My Cart: 3")
+
+    expect(page).to have_content("Cart Contents")
+    expect(page).to have_content(store_1.title)
+    expect(page).to have_content(store_2.title)
+    expect(page).to have_content(store_3.title)
+    expect(current_path).to eq(cart_path)
+
+  end
+end
