f7e4bdef6e2b51c059821f4f68dcd8c105ef38e4
Justin Pease <justinpease2@gmail.com>
2016-02-10 22:18:11 -0700

    adds test for user not logged in checking out

--- spec/features/user_must_log_in_before_checking_out_spec.rb
+++ spec/features/user_must_log_in_before_checking_out_spec.rb
@@ -1,25 +1,55 @@
 require 'rails_helper'
 
 RSpec.feature "user not logged in adds items to cart" do
-	scenario "clicks the checkout button" do
+	scenario "clicks the checkout button and is taken to the login page" do
+    user = create(:user)
 		store = Store.create(title: "test", accreditations: ["Hurray"])
-		category_1 = create_list(:category_with_items, 3)
-		item_1, item_2, item_3 = category_1[0].items
-		store.items << [item_1, item_2, item_3]
+		category_1 = create(:category)
+		item_1 = create(:item, category_id: category_1.id)
+		store.items << [item_1]
 
     visit store_path(store.slug)
-
     expect(page).to have_content("My Cart: 0")
 
 		first(:button, "Add To Cart").click
     expect(page).to have_content("My Cart: 1")
-    save_and_open_page
-    click_on()
-  end
-end
+    click_on("My Cart")
+    expect(page).to have_content(item_1.title)
 
-# as a user,
-# when I go shopping and I add items to my cart
-# when I click on the link to "checkout",
-# if I am not logged in,
-# I see an error message telling me that I need to log in before I can checkout
+    click_on("Checkout")
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Please log in to check out")
+  end
+
+  scenario "clicks the checkout button, then logs in and can checkout" do
+    user = create(:user)
+		store = Store.create(title: "test", accreditations: ["Hurray"])
+		category_1 = create(:category)
+		item_1 = create(:item, category_id: category_1.id)
+		store.items << [item_1]
+
+    visit store_path(store.slug)
+    expect(page).to have_content("My Cart: 0")
+
+		first(:button, "Add To Cart").click
+    expect(page).to have_content("My Cart: 1")
+    click_on("My Cart")
+    expect(page).to have_content(item_1.title)
+
+    click_on("Checkout")
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Please log in to check out")
+
+    fill_in("Username", with: user.username)
+    fill_in("Password", with: user.password)
+    click_on("Sign In")
+
+    expect(current_path).to eq(dashboard_path)
+    click_on("My Cart")
+    expect(page).to have_content(item_1.title)
+
+    click_on("Checkout")
+    expect(page).to have_content("Order was successfully placed.")
+  end
+
+end
