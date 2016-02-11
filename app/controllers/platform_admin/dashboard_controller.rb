3afeb42a47ce5f2cda5da8e1b63bb327d43d6f49
Justin Pease <justinpease2@gmail.com>
2016-02-10 17:19:33 -0700

    refactoring stores controller

--- app/controllers/platform_admin/dashboard_controller.rb
+++ app/controllers/platform_admin/dashboard_controller.rb
@@ -5,8 +5,9 @@ class PlatformAdmin::DashboardController < ApplicationController
   def index
     @categories = Category.all
     @stores = Store.all
-    @declined_stores = Store .where(status: "declined")
-    @pending_stores = Store.where(status: "pending")
+    # @declined_stores = Store .where(status: "declined")
+    # @pending_stores = Store.where(status: "pending")
     # @active_stores = Store.where(status: "active")
   end
+
 end
