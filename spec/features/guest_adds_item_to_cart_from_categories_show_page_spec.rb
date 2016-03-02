1465e57644d7c9985cf6ece7d5733b8bc2483f79
Justin Pease <justinpease2@gmail.com>
2016-03-01 22:01:29 -0700

    moves the javascript tags where it can be loaded at the bottom of the view appliaction

--- spec/features/guest_adds_item_to_cart_from_categories_show_page_spec.rb
+++ spec/features/guest_adds_item_to_cart_from_categories_show_page_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "guest visits category show page" do
 		item_1, item_2, item_3, item_4, item_5 = category_1[0].items
 		store.items << [item_1, item_2, item_3, item_4, item_5]
     guest = create(:user)
-    
+
     visit category_path(item_1.category.slug)
 
     expect(page).to have_content("My Cart: 0")
@@ -17,3 +17,4 @@ RSpec.feature "guest visits category show page" do
     expect(page).to have_content("My Cart: 1")
   end
 end
+	
